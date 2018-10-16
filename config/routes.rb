ef1ab1baa8f61d4c065180b693ca567b8ec8bdf8
Federico Esteban <federico.esteban@wolox.com.ar>
2018-10-16 20:20:55 -0300

    Added token model and endpoint.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root to: 'application#index'
 
+  resources :tokens, only: [] do
+    collection do
+      get :show
+    end
+  end
+
   mount Sidekiq::Web, at: 'sidekiq'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20181016225040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tokens", force: :cascade do |t|
+    t.string "friendly_id"
+    t.string "token_id"
+    t.string "session_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
